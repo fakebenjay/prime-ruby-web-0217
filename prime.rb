5508d719695554c67ca2aea4a026124e0b1e0a81
Ben Jay <fakebenjay@gmail.com>
2017-01-23 17:10:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,11 +1,21 @@
+## With help from johnshelb
+## The problem was not the each loop, but the else within the if/else within each
+## A for loop certainly looks more elegant here, but in the interest of honesty
+## I'm submitting it with the each, with the for loop commented out
+## a % operator can be used in place of .to_i and .to_f
+
 def prime?(number)
+  array = (2...number).to_a
+
   if number <= 1 || (number.to_f != number.to_i)
     return false
   elsif number == 2
     return true
   else
-    for divisor in (2...number)
+    array.each do |divisor|
       if ((number.to_f/divisor.to_f) == (number.to_i/divisor.to_i))
+    #for divisor in (2...number)
+      #if ((number.to_f/divisor.to_f) == (number.to_i/divisor.to_i))
         return false
       end
     end
