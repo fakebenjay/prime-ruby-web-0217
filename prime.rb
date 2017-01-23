11e6e8643bb3ec1e0b030cfd84958e4841d7ba92
Ben Jay <fakebenjay@gmail.com>
2017-01-23 17:03:41 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,17 +1,14 @@
 def prime?(number)
-  array = (2...number).to_a
-
   if number <= 1 || (number.to_f != number.to_i)
     return false
   elsif number == 2
     return true
   else
-    array.each do |element|
-      if ((number.to_f%element.to_f) == 0)
+    for divisor in (2...number)
+      if ((number.to_f/divisor.to_f) == (number.to_i/divisor.to_i))
         return false
-      else
-        return true
       end
     end
+  return true
   end
 end
